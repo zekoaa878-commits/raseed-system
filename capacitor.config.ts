0a5b258ba1fc907c521dde90465a01b888c40d90
zekoaa878-commits <zekoaa878@gmail.com>
2026-09-01 21:23:01 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,8 @@
-import { CapacitorConfig } from "@capacitor/cli"; const config: CapacitorConfig = { appId: "com.raseed.system", appName: "Raseed V4", webDir: ".", bundledWebRuntime: false }; export default config;
+import { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.raseed.system',
+  appName: 'Raseed V4',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+export default config;
