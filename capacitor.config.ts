34a0755803b2e82dfbe73edecebddf4333622cc2
zekoaa878-commits <zekoaa878@gmail.com>
2026-09-01 20:11:56 +0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.raseed.system',
-  appName: 'Raseed',
-  webDir: 'dist',
-  bundledWebRuntime: false
-};
-export default config;
