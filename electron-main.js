751804a25be16a395712370b36d81d00d9bcbd66
zekoaa878-commits <zekoaa878@gmail.com>
2026-09-01 20:11:31 +0300

    Delete electron-main.js

--- electron-main.js
+++
@@ -1,8 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-function createWindow(){
-  const win = new BrowserWindow({ width: 1200, height: 800, webPreferences: { nodeIntegration: false } });
-  win.loadFile(path.join(__dirname, 'dist', 'index.html'));
-}
-app.whenReady().then(createWindow);
-app.on('window-all-closed', ()=>{ if(process.platform!=='darwin') app.quit(); });
